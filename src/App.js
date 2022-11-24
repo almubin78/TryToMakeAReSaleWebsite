f7ecc6d4ab351101904236d41b0d37e20027ffba
Al Mubin <almubin78@gmail.com>
2022-11-24 10:53:26 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './router/routes';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
