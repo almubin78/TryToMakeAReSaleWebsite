9737890adb6eee43a16ab3d33b69ee68ae7cfa2d
Al Mubin <almubin78@gmail.com>
2022-11-30 19:17:07 +0600

    dynamically load data in Advertise section

--- src/components/shared/Advetise.js
+++ src/components/shared/Advetise.js
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
@@ -7,6 +8,15 @@ import { Link } from 'react-router-dom';
 const Advetise = () => {
     const [advertiseProducts, setAdvertiseProducts] = useState([]);
     const haveProduct = true;
+
+    const { data: sellers = [], refetch } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/sellers');
+            const data = await res.json();
+            return data;
+        }
+    })
     useEffect(() => {
         //এটি ছাড়া শুধু ফেচ করলেও ঠিক রেজাল্ট আসে। 
         fetch('http://localhost:5000/signleposts')
@@ -22,9 +32,6 @@ const Advetise = () => {
             {
                 haveProduct &&
                 <h1 className='text-center'> AvailAble Products {advertiseProducts.length}  </h1>
-
-
-
             }
             <hr className='text-primary fw-bold mb-3' />
             <Container>
@@ -49,27 +56,30 @@ const Advetise = () => {
                                         </Card.Text>
                                         <Card.Text>
                                             seller: {p.sellerName}
+                                            {
+                                                sellers.map(s => <span className='position-relative' key={s._id}>
+                                                    {
+                                                        s.verifiedSeller === true &&
+                                                        <span className="position-absolute top-50 start-100 translate-middle ms-3 p-2 bg-success border border-light rounded-circle">
+                                                            <span className="visually-hidden">New alerts</span>
+                                                        </span>
+                                                    }
+                                                </span>)
+                                            }
                                         </Card.Text>
                                         <Button variant="primary">Book Now</Button>
                                     </Card.Body>
                                 </Card>
-
-
-
                             )
-
                             :
-
                             <>
                                 <h3 className='text-center'> There are no Available Products </h3>
                                 <h4 className='text-center'>Waiting for Seller Post</h4>
                                 <h5 className='text-center'>Are You Interested to sold Out your Old Phone? Plz <Link className='text-decoration-none' to='register '>Register</Link> as a Seller</h5>
                             </>
-
                     }
                 </Row>
             </Container>
-
         </div>
     );
 };
